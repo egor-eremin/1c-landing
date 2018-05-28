c055d8fd65cdb3c41b3dce6d4609857f0a6bb78f
Egor Eremin <egor_eremin@mail.ru>
2018-05-29 03:44:14 +0800

    without contact

--- app/js/main.js
+++ app/js/main.js
@@ -154,5 +154,8 @@ $(document).ready(function() {
         $('.section-information').viewportChecker({
             classToAdd:'animated',
         });
+        $('.scheme').viewportChecker({
+            classToAdd:'animated',
+        });
     })();
 });
